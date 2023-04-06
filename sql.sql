0a60d4c34781ef2365a5c604a67bc2381cb64470
igorghirardelli <igorghirardelli@gmail.com>
2023-04-06 04:00:07 -0300

    table

---
+++ sql.sql
@@ -0,0 +1,18 @@
+--CREATE TABLE USERS (nome TEXT NOT NULL PRIMARY KEY, senha NOT NULL, email NOT NULL );
+
+--INSERT INTO  users ( nome, senha , email)
+--VALUES ("igor", "minhasenha1" , "ogsakdj@hotmail.com");
+
+--SELECT senha FROM users;
+
+--DELETE FROM users 
+
+
+
+
+
+
+
+
+
+
